4d6be1ea316a5f727b47796c207146ac808d6d11
Mahyar-bandegie82 <mahyarband1382@gmail.com>
2025-04-06 04:49:23 +0430

    alibaba replica
    alibaba homepage replica with htm l& css & javascript

---
+++ scripts/alibaba.js
@@ -0,0 +1,77 @@
+const list = document.getElementById("drop-down-list");
+const answer = document.getElementById('answer')
+
+function open_qa(list){
+    list.classList.toggle('open');
+    answer.classList.toggle('open-answer');
+}
+
+function open_qao(){
+    const answero = document.querySelector('.answero')
+    list.classList.toggle('open');
+    answero.classList.toggle('open-answer');
+}
+
+function open_qat(){
+    const answero = document.querySelector('.answert')
+    list.classList.toggle('open');
+    answero.classList.toggle('open-answer');
+}
+
+function open_qath(){
+    const answero = document.querySelector('.answerth')
+    list.classList.toggle('open');
+    answero.classList.toggle('open-answer');
+}
+
+function open_qaf(){
+    const answero = document.querySelector('.answerf')
+    list.classList.toggle('open');
+    answero.classList.toggle('open-answer');
+}
+function open_qafi(){
+    const answero = document.querySelector('.answerfi')
+    list.classList.toggle('open');
+    answero.classList.toggle('open-answer');
+}
+
+function open_qas(){
+    const answero = document.querySelector('.answers')
+    list.classList.toggle('open');
+    answero.classList.toggle('open-answer');
+}
+
+function footerdropdown(){
+    document.getElementById('dropdown').classList.toggle("footer-sub-menu");
+}
+function footerdropdowno(){
+    document.getElementById('dropdowno').classList.toggle("footer-sub-menu");
+}
+function footerdropdownt(){
+    document.getElementById('dropdownt').classList.toggle("footer-sub-menu");
+}
+function open__text(){
+    document.getElementById('open-text').style.whiteSpace = 'wrap';
+    document.getElementById('open-btn').style.display = 'none';
+    document.getElementById('close-btn').style.display = 'block';
+    document.getElementById('o-c-s').style.rotate = '180deg';
+}
+function close__text(){
+    document.getElementById('open-text').style.whiteSpace = 'nowrap';
+    document.getElementById('open-btn').style.display = 'block';
+    document.getElementById('close-btn').style.display = 'none';
+    document.getElementById('o-c-s').style.rotate = '360deg';
+}
+
+function open__textd(){
+    document.getElementById('open-textd').style.whiteSpace = 'wrap';
+    document.getElementById('open-btnd').style.display = 'none';
+    document.getElementById('close-btnd').style.display = 'block';
+    document.getElementById('o-c-s-d').style.rotate = '180deg';
+}
+function close__textd(){
+    document.getElementById('open-textd').style.whiteSpace = 'nowrap';
+    document.getElementById('open-btnd').style.display = 'block';
+    document.getElementById('close-btnd').style.display = 'none';
+    document.getElementById('o-c-s-d').style.rotate = '360deg';
+}
